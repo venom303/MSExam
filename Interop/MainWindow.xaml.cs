b2cb2550f34fed069ee9d0d03c6d6552d73dbd12
venom303 <venom303@gmail.com>
2013-10-01 23:57:08 +0200

    Interop

---
+++ Interop/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            var result = Win32NativeFunctions.GetClipboardData(0);
+            txtResult.Text = result.ToString();
+
+        }
+
+        private void btnSpace_Click(object sender, RoutedEventArgs e)
+        {
+            ulong freeBytes, totalBytes, totalFreeBytes;
+            if (!Win32NativeFunctions.GetDiskFreeSpaceEx(@"c:", out freeBytes, out totalBytes, out totalFreeBytes))
+            {
+                txtResult.Text = "Error occured: " + Marshal.GetExceptionForHR(Marshal.GetLastWin32Error()).Message;
+            }
+            else
+            {
+                txtResult.Text = "Free bytes: " + (freeBytes/1024/1024).ToString("n");
+            }
+        }
+
+        private void btnGetText_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr hwnd = Win32NativeFunctions.FindWindow("dwm", null);
+
+            var windows = Win32NativeFunctions.GetWindows();
+            var wdExpress = Process.GetProcessesByName("wdexpress").First();
+
+            var proc = Process.GetProcessById(6936);
+
+
+            var all = Process.GetProcesses();
+            var current = Process.GetCurrentProcess();
+
+            Win32NativeFunctions.SetWindowText(proc.MainWindowHandle, "Albo niedziala");
+
+            //var proc = Process.GetProcessesByName("dwm").First();
+            //while (proc.MainWindowHandle == IntPtr.Zero)
+            //{
+            //    // Discard cached information about the process
+            //    // because MainWindowHandle might be cached.
+            //    proc.Refresh();
+
+            //    Thread.Sleep(10);
+            //}
+
+
+            //var writer = new StringBuilder();
+            //Win32NativeFunctions.GetWindowText(wdExpress.MainWindowHandle, writer, 1000000);
+
+            //txtResult.Text = writer.ToString();
+        }
+    }
+}
---
+++ Interop/Win32NativeFunctions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interop
+{
+    public class Win32NativeFunctions
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetClipboardData(uint uFormat);
+
+        [DllImport("kernel32.dll", SetLastError=true, CharSet=CharSet.Ansi)]
+        public static extern bool GetDiskFreeSpaceEx(string lpDirectoryName, out ulong lpFreeBytesAvailable, out ulong lpTotalNumberOfBytes, out ulong lpTotalNumberOfFreeBytes);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowText(IntPtr hWnd, string text);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        
+        
+        public delegate bool EnumedWindow(IntPtr handleWindow, ArrayList handles);
+
+        
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumedWindow lpEnumFunc, ArrayList lParam);
+
+        public static ArrayList GetWindows()
+        {
+            ArrayList windowHandles = new ArrayList();
+            
+            EnumWindows(GetWindowHandle, windowHandles);
+
+            return windowHandles;
+        }
+
+        private static bool GetWindowHandle(IntPtr windowHandle, ArrayList windowHandles)
+        {
+            windowHandles.Add(windowHandle.ToInt32());
+            return true;
+        }
+    }
+}
